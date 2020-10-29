90e446d0f11938c7a73c6a5aa4e45b6a7791efad
martycook <martinc@valornet.com>
2020-10-29 17:20:09 -0500

    adding options

---
+++ src/CG.Hosting/Options/HostOptions.cs
@@ -0,0 +1,27 @@
+﻿using CG.Options;
+using CG.Serilog.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG.Hosting.Options
+{
+    /// <summary>
+    /// This class represents configuration settings for hosting.
+    /// </summary>
+    public class HostOptions : OptionsBase
+    {
+        // *******************************************************************
+        // Properties.
+        // *******************************************************************
+
+        #region Properties
+
+        /// <summary>
+        /// This property contains logging options.
+        /// </summary>
+        public SerilogOptions Logging { get; set; }
+
+        #endregion
+    }
+}
